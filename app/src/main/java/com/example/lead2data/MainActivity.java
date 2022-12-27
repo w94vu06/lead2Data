97f05c76b9f8c1989649b6c5ce47678f5f58d698
doob925 <doob925@gmail.com>
2022-12-27 15:18:18 +0800

    讀取CHA 50%Finish 嘗試模組化

--- app/src/main/java/com/example/lead2data/MainActivity.java
+++ app/src/main/java/com/example/lead2data/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.res.AssetFileDescriptor;
 import android.content.res.AssetManager;
@@ -16,7 +18,11 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 
+import com.codekidlabs.storagechooser.StorageChooser;
+
+import org.apache.commons.lang3.ArrayUtils;
 import org.tensorflow.lite.Interpreter;
+import org.tensorflow.lite.Tensor;
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -24,6 +30,7 @@ import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 
@@ -31,107 +38,297 @@ import ir.androidexception.filepicker.dialog.SingleFilePickerDialog;
 
 
 public class MainActivity extends AppCompatActivity {
+    private int[] shape;
     private TextView textOutput;
     private Button btnFileInput;
     private Button btnFileOutput;
+    private Button btnFileOutput2;
     private ReadCSVThread readCSVThread;
     private ArrayList<String> csvDataList = new ArrayList<>();
-    private ArrayList<Float> csvDataListFloat = new ArrayList<Float>();
-    private ArrayList<Float> csvDataListFloatFinish = new ArrayList<>();
-
-    private List<Float> listFloat = new ArrayList<Float>() {
-    };
-    private List<Float> listFloat2 = new ArrayList<Float>() {
-    };
-    private List<Float> listFloatPartA = new ArrayList<Float>() {
-    };
-    private List<Float> listFloatPartB = new ArrayList<Float>() {
-    };
-    private int[] shape;
-    private float[] input1Array;
-    private float[] input2Array;
-    private float[][] array;
     private float[][][] reshapedArray;
-
+    private float[][][] reshapedArray2;
     private final String mModelPath = "mymodel.tflite";
+    private final String mModelPath_loose = "mymodel_loose.tflite";
     private Interpreter interpreter;
+    private String checkSignal;
+    private String getPath;
+    private CellData cell;
+    ArrayList sample2 = new ArrayList();
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textOutput = findViewById(R.id.textOutput);
-        btnFileInput = findViewById(R.id.btnFileInput);
-        btnFileOutput = findViewById(R.id.btnFileOutput);
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
-                && checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                != PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1000);
-        }
-//        mHandler = new MHandler();
-
         try {
-            readCSV();
+            init();
+            initPermission();
             initInterpreter();
             initPredict();
+            initChooser();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public void initPredict() {
-        btnFileOutput.setOnClickListener(new View.OnClickListener() {
+    public void init() {
+        textOutput = findViewById(R.id.textOutput);
+        btnFileInput = findViewById(R.id.btnFileInput);
+        btnFileOutput = findViewById(R.id.btnFileOutput);
+        btnFileOutput2 = findViewById(R.id.btnFileOutput2);
+    }
+    /** 檢查權限*/
+    public void initPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1000);
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+        }
+    }
+    /** 檔案選擇器*/
+    public void initChooser() {
+        btnFileInput.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                float[] result = doInference(reshapedArray);
-                Log.d("bbbb", "onClick: " + Arrays.toString(result));
-                                runOnUiThread(() -> {
-                    textOutput.setText(Arrays.toString(result));
+                StorageChooser chooser = new StorageChooser.Builder()
+                        .withActivity(MainActivity.this)
+                        .withFragmentManager(getFragmentManager())
+                        .withMemoryBar(true)
+                        .allowCustomPath(true)
+                        .setType(StorageChooser.FILE_PICKER)
+                        .build();
+                chooser.show();
+
+                chooser.setOnSelectListener(new StorageChooser.OnSelectListener() {
+                    @Override
+                    public void onSelect(String path) {
+                        if (path.contains(".csv")) {
+                            openCSV(path);
+                        }
+                        if (path.contains(".CHA")) {
+                            readCHA(path);
+                        }
+                        getPath = path;
+
+                    }
                 });
             }
         });
     }
-
-    public void initInterpreter() throws Exception {
+    /** 直譯器*/
+    public void initInterpreter() throws IOException {
         Interpreter.Options options = new Interpreter.Options();
-        options.setNumThreads(4);
+        options.setNumThreads(8);
         options.setUseNNAPI(true);
-        interpreter = new Interpreter(loadModelFile(getAssets(), mModelPath), options);
-
-        interpreter.getInputTensor(0);
-        int input_tensor_count = interpreter.getInputTensorCount();
-        // 遍歷每個輸入張量
-        for (int i = 0; i < input_tensor_count; i++) {
-            // 獲取輸入張量的形狀
-            shape = interpreter.getInputTensor(i).shape();
-            Log.d("gggg", "initInterpreter: " + shape);
-        }
-
-//        int output_tensor_count = interpreter.getOutputTensorCount();
-//        // 遍歷每個輸入張量
-//        for (int i = 0; i < output_tensor_count; i++) {
-//            // 獲取輸出張量的形狀
-//            int[] shape2 = interpreter.getOutputTensor(i).shape();
-//            Log.d("gggg", "init: "+Arrays.toString(shape2));
-//        }
-//        Tensor filterTensor = interpreter.getInputTensor(0);
-//        int[] shape3 = filterTensor.shape();
-//        for (int i = 0; i < shape3.length; i++) {
-//            Log.d("gggg", "sf: "+Arrays.toString(shape3));
-//        }
+        /** 換模 */
+//        interpreter = new Interpreter(loadModelFile(getAssets(), mModelPath), options);
+        interpreter = new Interpreter(loadModelFile(getAssets(), mModelPath_loose), options);
     }
 
-    public float[] doInference(float[][][] daa) {
+    public void initPredict() {
+        /** 預測一段 */
+        btnFileOutput.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                long startTime = System.currentTimeMillis();
+                float[] result = doInference(reshapedArray);
+                for (float f : result) {
+                    if (f < 0.5) {
+                        checkSignal = "壞訊號";
+                    } else {
+                        checkSignal = "好訊號";
+                    }
+                }
+                runOnUiThread(() -> {
+                    long endTime = System.currentTimeMillis();
+                    float elapsedTime = (endTime - startTime) / 1000;
+                    textOutput.setText(Arrays.toString(result) + "\n" + checkSignal + "\n使用" + elapsedTime + "秒");
+                });
+            }
+        });
+        /** 預測兩段 */
+        btnFileOutput2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                long startTime = System.currentTimeMillis();
+                float[] result = doInference(reshapedArray);
+                float[] result2 = doInference(reshapedArray2);
+                float[] combined = ArrayUtils.addAll(result, result2);
+                boolean isGood = true;
+                for (float f : combined) {
+                    if (f < 0.5) {
+                        isGood = false;
+                    }
+                    break;
+                }
+                if (isGood) {
+                    checkSignal = "好訊號";
+                } else {
+                    checkSignal = "壞訊號";
+                }
+                runOnUiThread(() -> {
+                    long endTime = System.currentTimeMillis();
+                    float elapsedTime = (endTime - startTime) / 1000;
+                    textOutput.setText(Arrays.toString(combined) + "\n" + checkSignal + "\n使用" + elapsedTime + "秒");
+                });
+            }
+        });
+    }
+    /** 預測*/
+    public float[] doInference(float[][][] data) {
+
         float[][] outputs = new float[1][1];
-        interpreter.run(daa,outputs);
+        interpreter.run(data, outputs);
 
         float[] outputArray = new float[1];
         outputArray[0] = outputs[0][0];
         return outputArray;
     }
+    /** 讀CHA*/
+    public void readCHA(String fileName) {
+        try {
+            int x = 64;
+            char[] a = new char[32 * 1024 * 1024];
+            int cha_size = LcndUtil.len(getPath, a);
+            byte[] content = LcndUtil.readFromByteFile(getPath);
+            /** Lead1 */
+            ArrayList CHA_LI_dataNum = new ArrayList();
+            ArrayList CHA_LI_data16 = new ArrayList();
+            ArrayList CHA_LI_dataSample = new ArrayList();
+            ArrayList CHA_LI_data4 = new ArrayList();
+            byte[] c1 = new byte[cha_size];
+            for (int L1 = 0; L1 < cha_size; L1 += 136) {
+                int y = L1 + x;
+                int j = 0;
+                for (int k = L1; k < y; k++) {
+                    c1[j] = content[k];
+                    j++;
+                }
+                oneCellData(c1, c1.length, 1);
+                CHA_LI_dataNum.add(cell.getLen());
+                CHA_LI_data16.add(cell.getList(1));
+                CHA_LI_dataSample.add(cell.getList(2));
+                CHA_LI_data4.add(cell.getList(3));
+            }
+            int startS = 0;
+            double dou = Math.floor((startS * 1000) / 32);
+            int pic = (int) dou;
+            double sample_len = CHA_LI_dataSample.size();
+            double dou_2 = (sample_len / 32) * 31 - 5;
+            int picEnd = (int) dou_2;
 
+            /** Lead2 */
+            ArrayList CHA_LII_dataNum = new ArrayList();
+            ArrayList CHA_LII_data16 = new ArrayList();
+            ArrayList CHA_LII_dataSample = new ArrayList();
+            ArrayList CHA_LII_data4 = new ArrayList();
+
+            byte[] c2 = new byte[cha_size];
+            for (int L2 = 64; L2 < cha_size; L2 += 136) {
+                int y = L2 + x;
+                int j = 0;
+                for (int k = L2; k < y; k++) {
+                    c2[j] = content[k];
+                    j++;
+                }
+                oneCellData(c2, c2.length, 2);
+                CHA_LII_dataNum.add(cell.getLen());
+                CHA_LII_data16.add(cell.getList(1));
+                CHA_LII_dataSample.add(cell.getList(2));
+                CHA_LII_data4.add(cell.getList(3));
+            }
+            for (int i = pic; i < picEnd; i++) {
+                List s = Arrays.asList(CHA_LII_dataSample.get(i));
+                int y = s.get(0).toString().length();
+                String f = s.get(0).toString().substring(1, y - 1).replaceAll(" ", "");
+                List<String> myList = new ArrayList<>(Arrays.asList(f.split(",")));
+                sample2.addAll(myList);
+            }
+            for (int i = 0; i < sample2.size(); i++) {
+                int k = Integer.parseInt(sample2.get(i).toString());
+                sample2.set(i, ((k - 2048) * 5) * 0.001);
+            }
+        } catch (Exception ignored) {
+
+        }
+    }
+
+    public void oneCellData(byte[] cha, int len, int c) {
+        ArrayList<String> onecell = new ArrayList();
+        for (int i = 0; i < 64; i++) {
+            String newcha = Integer.toBinaryString(cha[i]);
+            int wordnum = newcha.length();
+            if (wordnum != 8) {
+                for (int k = 0; k < (8 - wordnum); k++) {
+                    String x = '0' + newcha;
+                    newcha = x;
+                }
+                if (wordnum > 8) {
+                    String x = newcha.substring(24);
+                    newcha = x;
+                }
+            } else {
+                newcha = Integer.toBinaryString(cha[i]);
+                String x = newcha.substring(0, 8);
+                newcha = x;
+            }
+            onecell.add(newcha);
+        }
+
+        ArrayList<String> newCell = new ArrayList<>();
+        for (int i = 0; i < 64; i += 2) {
+            String low = onecell.get(i);
+            String hi = onecell.get(i + 1);
+            String new16 = hi + low;
+            newCell.add(new16);
+        }
+
+        ArrayList<Integer> spv = new ArrayList<>();
+        ArrayList<String> dataV = new ArrayList<>();
+        for (int i = 0; i < 32; i++) {
+            String sp = newCell.get(i).substring(4, 16);
+            String dat = newCell.get(i).substring(0, 4);
+            int x = Integer.parseInt(sp, 2);
+            spv.add(x);
+            dataV.add(dat);
+        }
+        cell = new CellData(len, newCell, spv, dataV);
+    }
+    /** 讀CSV*/
+    private void openCSV(String fileName) {
+        if (fileName != "") {
+            readCSVThread = new ReadCSVThread(fileName);
+            readCSVThread.run();
+            List<Float> floatData = new ArrayList<>(readCSVThread.csvFloat);
+            textOutput.setText("匯入資料成功");
+            float[] inputArray = new float[floatData.size()];
+            for (int i = 0; i < floatData.size(); i++) {
+                inputArray[i] = floatData.get(i);
+            }
+
+            float[] rawSignalFirst = Arrays.copyOfRange(inputArray, 0, 12000);
+            float[] rawSignalSecond = Arrays.copyOfRange(inputArray, 12000, 24000);
+            Log.d("checkkkk", "openCSV: " + rawSignalSecond.length);
+
+            reshapedArray = new float[1][12000][1];
+            for (int i = 0; i < 12000; i++) {
+                reshapedArray[0][i][0] = rawSignalFirst[i];
+            }
+            reshapedArray2 = new float[1][12000][1];
+            for (int i = 1; i < 12000; i++) {
+                reshapedArray2[0][i][0] = rawSignalSecond[i];
+            }
+        } else {
+            textOutput.setText("PLS INPUT!!");
+        }
+    }
+    /** 讀TFLite模*/
     private MappedByteBuffer loadModelFile(AssetManager assetManager, String modelPath) throws IOException {
         AssetFileDescriptor fileDescriptor = assetManager.openFd(modelPath);
         FileInputStream inputStream = new FileInputStream(fileDescriptor.getFileDescriptor());
@@ -140,77 +337,4 @@ public class MainActivity extends AppCompatActivity {
         long declaredLength = fileDescriptor.getDeclaredLength();
         return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength);
     }
-
-    public void readCSV() {
-        btnFileInput.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                SingleFilePickerDialog singleFilePickerDialog = new SingleFilePickerDialog(MainActivity.this,
-                        () -> textOutput.setText("Canceled!!"),
-                        files -> openCSV(files[0].getPath()));
-                singleFilePickerDialog.show();
-            }
-        });
-    }
-
-    private void openCSV(String fileName) {
-        if (fileName != "") {
-            readCSVThread = new ReadCSVThread(fileName);
-            readCSVThread.run();
-            csvDataList.clear();
-            csvDataList.addAll(readCSVThread.csvArray);
-            csvDataList.remove(0);
-            if (csvDataList.size() > 24000) {
-                int selectTime = (csvDataList.size() - 24000) / 2;
-
-                for (int i = 0; i < csvDataList.size(); i++) {
-                    csvDataListFloat.add(Float.valueOf(csvDataList.get(i)));
-                }
-                listFloat = csvDataListFloat.subList(selectTime, csvDataListFloat.size() - selectTime);
-                listFloat2 = listFloat.subList(0, 24000);
-                listFloatPartA = listFloat2.subList(0, 12000);
-                listFloatPartB = listFloat2.subList(12000, 24000);
-
-                input1Array = new float[listFloatPartA.size()];
-                for (int i = 0; i < listFloatPartA.size(); i++) {
-                    input1Array[i] = listFloatPartA.get(i);
-                }
-                input2Array = new float[listFloatPartB.size()];
-                for (int i = 0; i < listFloatPartB.size(); i++) {
-                    input2Array[i] = listFloatPartB.get(i);
-                }
-                float[] rawSignalSubarray = Arrays.copyOfRange(input1Array, 0, 12000);
-                reshapedArray = new float[1][12000][1];
-                for (int i = 0; i < 12000; i++) {
-                    reshapedArray[0][i][0] = rawSignalSubarray[i];
-                }
-
-
-            } else {
-                Toast.makeText(this, "資料錯誤", Toast.LENGTH_LONG).show();
-            }
-            textOutput.setText("Success!!");
-        } else {
-            textOutput.setText("PLS INPUT!!");
-        }
-
-    }
-
-    /**
-     * 接收線程裡的資料
-     */
-//    class MHandler extends Handler {
-//        @Override
-//        public void handleMessage(@NonNull Message msg) {
-//            super.handleMessage(msg);
-//            switch (msg.what) {
-//                case MESSAGE_TYPE_1:
-//                    backData = (float[]) msg.obj;
-//                    break;
-//                case MESSAGE_TYPE_2:
-//                    backData2 = (float[]) msg.obj;
-//                    break;
-//            }
-//        }
-//    }
 }
--- app/src/main/java/com/example/lead2data/ReadCSVThread.java
+++ app/src/main/java/com/example/lead2data/ReadCSVThread.java
@@ -1,20 +1,24 @@
 package com.example.lead2data;
 
+import android.content.Context;
+import android.widget.Toast;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class ReadCSVThread extends Thread{
     String fileName;
+    List<Float> csvFloat = new ArrayList<>();
     ArrayList<String> csvArray = new ArrayList<>();
-
     public ReadCSVThread(String fileName) {
         this.fileName = fileName;
     }
-
     @Override
     public void run() {
         super.run();
@@ -23,19 +27,36 @@ public class ReadCSVThread extends Thread{
             BufferedReader reader = new BufferedReader(new FileReader(csvFile));
             String csvString;
             String[] arrayFinish;
-            while ((csvString = reader.readLine()) != null) {
-                arrayFinish = csvString.split(",");
-                for (String arrayFinishData : arrayFinish) {
-                    csvArray.add(arrayFinishData);
+            csvString = reader.readLine();
+            arrayFinish = csvString.split(",");
+            int titleIndex = -1;
+            for (int i = 0; i < arrayFinish.length; i++) {
+                if (arrayFinish[i].equals("Lead2")) {
+                    titleIndex = i;
+                    break;
                 }
             }
+            while ((csvString = reader.readLine()) != null ) {
+                arrayFinish = csvString.split(",");
+                if (titleIndex >= 0 && titleIndex < arrayFinish.length) {
+                    csvArray.add(arrayFinish[titleIndex]);
+                }
+
+            }
             reader.close();
-        } catch (FileNotFoundException e) {
+            csvArray.remove(0);
+            for (int i = 0; i < csvArray.size(); i++) {
+                csvFloat.add(Float.valueOf(csvArray.get(i)));
+            }
+            if (csvArray.size() >= 24000) {
+                int extraTime = (csvFloat.size() - 24000) / 2;
+                csvFloat = csvFloat.subList(extraTime, csvFloat.size() - extraTime);
+                csvFloat = csvFloat.subList(0, 24000);
+            } else {
+                throw new Exception("資料錯誤");
+            }
+        } catch (Exception e) {
             e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (Exception error) {
-            error.printStackTrace();
         }
     }
 }
